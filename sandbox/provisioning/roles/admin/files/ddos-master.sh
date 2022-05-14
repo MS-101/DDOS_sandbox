9bf6d503e386bdba18f06bc3c84a8855f6cd66c5
Lings are cute <martin.svab@gmail.com>
2022-05-14 21:06:01 +0200

    final commit

--- sandbox/provisioning/roles/admin/files/ddos-master.sh
+++ sandbox/provisioning/roles/admin/files/ddos-master.sh
@@ -2,12 +2,18 @@
 
 MYARG=$1
 
-if [ $MYARG = "ping-test" ]
+if [ $MYARG = "ping-test-ip" ]
 then
 	ssh vagrant@10.10.20.10 ping -c 20 10.10.10.10
-elif [ $MYARG = "w3m-test" ]
+elif [ $MYARG = "ping-test-domain" ]
+then
+	ssh vagrant@10.10.20.10 ping -c 20 ddos.com
+elif [ $MYARG = "w3m-test-ip" ]
 then
 	ssh vagrant@10.10.20.10 w3m 10.10.10.10
+elif [ $MYARG = "w3m-test-domain" ]
+then
+	ssh vagrant@10.10.20.10 w3m ddos.com
 else
 	ssh botmaster@10.10.30.10 /home/botmaster/bot-controller.sh $MYARG &
 fi
